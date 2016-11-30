8851a124637947dec621114b959cd69e0a102c8f
Zubizerat <omeryentur@gmail.com>
2016-11-29 22:04:19 -0500

    Initial commit

---
+++ Commentaryv10/Commentaryv10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Commentaryv10.MvcApplication" Language="C#" %>
---
+++ Commentaryv10/Commentaryv10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Commentaryv10.Startup))]
+namespace Commentaryv10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
